bae5eac634b57eca0dcd4354969055545b87a6de
Baskoro Nugroho <baskoro.nugroho@telkom.co.id>
2025-11-13 21:59:52 +0700

    refactor: make front navbar login link to auth and include navbar in home layout

---
+++ larapp/resources/views/components/home/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="nav" role="navigation" aria-label="Menu utama">
+  <div class="nav-inner">
+    <ul>
+      <li><a class="active" href="#">Beranda</a></li>
+      <li><a href="#">Masjid</a></li>
+      <li><a href="#">Mushalla</a></li>
+      <li><a href="#">Info Terkini</a></li>
+      <li><a href="#">Unduh Data</a></li>
+      <li><a href="#">Kontak Kami</a></li>
+    </ul>
+    <a href="{{ route('login') }}" class="login" role="button">Login</a>
+  </div>
+</nav>
