6db03d27f66d4f45e4ce7629e028c045a321dbd5
Andri Marine <165137960+marinekarandri@users.noreply.github.com>
2025-11-30 20:20:57 +0700

    ui: hero text color -> #212529 for stronger contrast

--- larapp/resources/views/components/home/beranda/_hero.blade.php
+++ larapp/resources/views/components/home/beranda/_hero.blade.php
@@ -1,6 +1,6 @@
 <main class="container pb-0">
 	<div class="hero-card mt-1" style="background-image:url('{{ asset('images/mosque.webp') }}')">
-		<div class="hero-content" style="color:#374151; text-shadow:none;">
+		<div class="hero-content" style="color:#212529; text-shadow:none;">
 			{{$slot}}
 		</div>
 	</div>
