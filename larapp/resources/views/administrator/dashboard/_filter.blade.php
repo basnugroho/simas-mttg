0e3a7b2d0b00511640c3bd9c3c3b6a7a0db7c96a
Andri Marine <165137960+marinekarandri@users.noreply.github.com>
2025-12-08 13:35:11 +0700

    fix mobile view filter dashboard

--- larapp/resources/views/administrator/dashboard/_filter.blade.php
+++ larapp/resources/views/administrator/dashboard/_filter.blade.php
@@ -3,17 +3,28 @@
         <div class="card p-2 shadow-sm">
           <div class="d-flex align-items-center justify-content-between">
             <div>
-              <h5 class="mb-0">Peta Sebaran Masjid / Musholla</h5>
-              <small class="text-muted">Filter cepat untuk menampilkan data pada tabel dan peta</small>
+              <h6 class="mb-0">Peta Sebaran Masjid / Musholla</h6>
             </div>
             <div>
-              <form id="facFiltersForm" method="GET" action="" class="d-flex gap-2 align-items-center">
-                <select id="facRegional" name="regional_id" class="form-select form-select-sm" style="min-width:140px;"><option value="">Semua Regional</option></select>
-                <select id="facArea" name="area_id" class="form-select form-select-sm" style="min-width:140px;" disabled><option value="">Semua Area</option></select>
-                <select id="facWitel" name="witel_id" class="form-select form-select-sm" style="min-width:140px;" disabled><option value="">Semua Witel</option></select>
-                <select id="facSto" name="sto_id" class="form-select form-select-sm" style="min-width:140px;" disabled><option value="">Semua STO</option></select>
-                <select id="facType" name="type" class="form-select form-select-sm" style="min-width:120px;"><option value="">Jenis (Semua)</option><option value="masjid">Masjid</option><option value="mushalla">Mushalla</option></select>
-                <button id="facReset" type="button" class="btn btn-sm btn-outline-secondary">Reset</button>
+              <form id="facFiltersForm" method="GET" action="" class="row gx-2 gy-2 align-items-center">
+                <div class="col-12 col-sm-auto">
+                  <select id="facRegional" name="regional_id" class="form-select form-select-sm"><option value="">Semua Regional</option></select>
+                </div>
+                <div class="col-12 col-sm-auto">
+                  <select id="facArea" name="area_id" class="form-select form-select-sm" disabled><option value="">Semua Area</option></select>
+                </div>
+                <div class="col-12 col-sm-auto">
+                  <select id="facWitel" name="witel_id" class="form-select form-select-sm" disabled><option value="">Semua Witel</option></select>
+                </div>
+                <div class="col-12 col-sm-auto">
+                  <select id="facSto" name="sto_id" class="form-select form-select-sm" disabled><option value="">Semua STO</option></select>
+                </div>
+                <div class="col-12 col-sm-auto">
+                  <select id="facType" name="type" class="form-select form-select-sm"><option value="">Jenis (Semua)</option><option value="masjid">Masjid</option><option value="mushalla">Mushalla</option></select>
+                </div>
+                <div class="col-12 col-sm-auto">
+                  <button id="facReset" type="button" class="btn btn-sm btn-outline-secondary">Reset</button>
+                </div>
               </form>
             </div>
           </div>
